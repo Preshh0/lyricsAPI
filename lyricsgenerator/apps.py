738554f168734bda47cafd3a09081ce2dfec5cfc
Preshh0 <uniquep201@gmail.com>
2022-02-01 19:27:46 +0100

    Initial commit for the lyrics generator app. Also added the template file.

---
+++ lyricsgenerator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LyricsgeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lyricsgenerator'
---
+++ lyricsgenerator/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+def home(requests):
+   return render (requests, "index.html")
